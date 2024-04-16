16386fee0b9022010897da8367c01627aeb3efbc
CoderDonna <20enot0412@mail.ru>
2024-04-16 13:59:01 +0300

    Refactor for better maintainability_e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :ImgProcessor="ImgProcessor" />
+</template>
+
+<script>
+import Vue from 'vue'
+import ImgProcessor from '@ImgProcessor/core'
+import { Dashboard } from '@ImgProcessor/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    ImgProcessor: () => new ImgProcessor(),
+  },
+}
+</script>
+
+<style src="@ImgProcessor/core/dist/style.css"></style>
+<style src="@ImgProcessor/dashboard/dist/style.css"></style>
