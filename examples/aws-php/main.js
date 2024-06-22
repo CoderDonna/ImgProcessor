f43a48e0d4912d7b400cadbe045640eb7dce2a12
CoderDonna <20enot0412@mail.ru>
2024-06-22 23:27:01 +0300

    Committing examples/aws-php/main.js

---
+++ examples/aws-php/main.js
@@ -0,0 +1,42 @@
+import ImgProcessor from '@ImgProcessor/core'
+import Dashboard from '@ImgProcessor/dashboard'
+import AwsS3 from '@ImgProcessor/aws-s3'
+
+const ImgProcessor = new ImgProcessor({
+  debug: true,
+})
+
+ImgProcessor.use(Dashboard, {
+  inline: true,
+  target: 'body',
+})
+ImgProcessor.use(AwsS3, {
+  shouldUseMultipart: false, // The PHP backend only supports non-multipart uploads
+
+  getUploadParameters (file) {
+    // Send a request to our PHP signing endpoint.
+    return fetch('/s3-sign.php', {
+      method: 'post',
+      // Send and receive JSON.
+      headers: {
+        accept: 'application/json',
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        filename: file.name,
+        contentType: file.type,
+      }),
+    }).then((response) => {
+      // Parse the JSON response.
+      return response.json()
+    }).then((data) => {
+      // Return an object in the correct shape.
+      return {
+        method: data.method,
+        url: data.url,
+        fields: data.fields,
+        headers: data.headers,
+      }
+    })
+  },
+})
